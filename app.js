a81800ac17b99c1b0c1d6e15af849287ac95228b
guru-bandike <guru.bandike@gmail.com>
2024-05-04 20:08:31 +0530

    Add basic user authentication

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ import express from 'express';
 
 // Import necessary internal modules
 import productRouter from './src/features/product/product.routes.js';
+import userRouter from './src/features/user/user.routes.js';
+import basicAuthorizer from './src/middlewares/basicAuth.middleware.js';
 
 // Create Express server instance
 const app = express();
 
 app.use(express.json()); // Parse incoming JSON bodies
 // Mount the productRouter for handling product related routes
-app.use('/api/products/', productRouter);
+app.use('/api/products/', basicAuthorizer, productRouter);
+// Mount the userRouter for handling user related routes
+app.use('/api/user/', userRouter);
 
 app.get('/', (req, res) => {
     res.send('Welcome to E-Commerce API')
---
+++ src/features/user/user.controller.js
@@ -0,0 +1,31 @@
+import UserModel from './user.model.js';
+
+// Define User Controller and export as default
+export default class UserController {
+  // Handle user signup request
+  signUp(req, res) {
+    const { name, email, password, role } = req.body; // Extract user information from request body
+    const newuser = UserModel.signUp(name, email, password, role); // Sign up using user model and get newly created user
+
+    // Create response object
+    const response = {
+      message: 'User signed up successfully',
+      user: newuser,
+    };
+
+    res.status(201).send(response); // Send response to the client
+  }
+
+  // Handle user signin request
+  signIn(req, res) {
+    const {email, password} = req.body; // Extract user credentials from request body
+
+    const foundUser = UserModel.signIn(email, password);
+
+    if(!foundUser) {
+      return res.status(400).send('Invalid User Credentials!')
+    }
+
+    res.status(200).send('User signed in successfully!');
+  }
+}
